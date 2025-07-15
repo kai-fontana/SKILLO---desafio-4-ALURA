eb186099d45b82efc072744f7730a10e3448f65a
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-07-15 08:29:04 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
 //DESAFIO 01
-//let mensagemDeBoasVindas = "Boas-vindas!";
-//console.log(mensagemDeBoasVindas);
+let mensagemDeBoasVindas = "Boas-vindas!";
+console.log(mensagemDeBoasVindas);
 
 //DESAFIO 02 
-//let nome = Kai;
-//console.log(`Olá, ${nome}`);
+let nome = Kai;
+console.log(`Olá, ${nome}`);
 
 //DESAFIO 03
-//let nome2 = Kaillanny;
-//alert(`Olá, ${nome2}`);
+let nome2 = Kaillanny;
+alert(`Olá, ${nome2}`);
 
 //DESAFIO 04
-//let lingProgrFavorita = prompt("Qual a linguagem de programação que você mais gosta?");
-//console.log(lingProgrFavorita);
+let lingProgrFavorita = prompt("Qual a linguagem de programação que você mais gosta?");
+console.log(lingProgrFavorita);
 
 //DESAFIO 05
-//let valor1 = 5;
-//let valor2 = 8;
-//let resultado = valor1 + valor2;
-
+let valor1 = 5;
+let valor2 = 8;
+let resultado = valor1 + valor2;
+ 
 //alert(resultado);
-//console.log(`A soma entre ${valor1} e ${valor2} é igual a ${resultado}.`);
+console.log(`A soma entre ${valor1} e ${valor2} é igual a ${resultado}.`);
 
-//DESAFIO 06
-//let valor01 = 9;
-//let valor02 = 6;
-//let resultado1 = valor01 - valor02;
-//console.log(`A diferença entre ${valor01} e ${valor02} é igual a ${resultado}.`);
+ //DESAFIO 06
+let valor01 = 9;
+let valor02 = 6;
+let resultado1 = valor01 - valor02;
+console.log(`A diferença entre ${valor01} e ${valor02} é igual a ${resultado}.`);
 
 //DESAFIO 07
-//let idadeDoUsuario = prompt("Insira a sua idade: ");
-//if (idadeDoUsuario > 18) {
-//    console.log("Você é maior de idade!");
-//} else{
-//    console.log("Você é menor de idade!");
-//}
+let idadeDoUsuario = prompt("Insira a sua idade: ");
+if (idadeDoUsuario > 18) {
+    console.log("Você é maior de idade!");
+} else{
+    console.log("Você é menor de idade!");
+}
 
 //DESAFIO 08
 let numero = prompt("Insira um valor: ");
@@ -43,7 +43,7 @@ if(numero == 0) {
     alert("O número é zero.");
 }
 
-//O LET COMPARACAONUMEROS É QUE ESTÁ ABAIXO só que sem ternário
+//VERSÃO ESTENDIDA DO OPERADOR TERNÁRIO ACIMA
 //if(numero > 0) {
 //    alert("O número é positivo");
 //} else {
@@ -56,7 +56,7 @@ if(numero == 0) {
 //DESAFIO 09
 let numeroInicial = 1;
 while(numeroInicial <= 10) {
-    alert(numeroInicial);
+    console.log(numeroInicial);
     numeroInicial++;
 }
 
@@ -65,5 +65,21 @@ let nota = 4;
 let comparacaoNota = nota >= 7 ? "Aprovado" : "Reprovado";
 console.log(comparacaoNota);
 
+//VERSÃO ESTENDIDA DO OPERADOR TERNÁRIO ACIMA
+//if (nota >= 7) {
+//    console.log("Aprovado");
+//} else{
+//    console.log("Reprovado");
+//}
+
 //DESAFIO 11
-Math.random
+let numeroAleatorio = parseInt(Math.random() * 10);
+console.log(numeroAleatorio);
+
+//DESAFIO 12
+let numeroAleatorio1 = Math.floor(Math.random() * 10) + 1;
+console.log(numeroAleatorio1);
+
+//DESAFIO 13
+let numeroAleatorio2 = Math.floor(Math.random() * 100) +1;
+console.log(numeroAleatorio2);
